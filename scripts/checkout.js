a49e8f07e6870ec4405ba0ea02e053b1eb41bd4b
ZiadGH <ziad97algawy@gmail.com>
2025-11-22 14:51:20 +0100

    14 F-G update button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -37,9 +37,12 @@ cart.forEach((cartItem) => {
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link"
+            data-product-id="${matchingProduct.id}">
               Update
             </span>
+            <input class="quantity-input";>
+            <span class="save-quantity-link link-primary";>Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -118,3 +121,11 @@ document.querySelector('.js-return-to-home-link')
   .innerHTML = `${cartQuantity} items`;
 }
 updateCartQuantity();
+
+document.querySelectorAll('.js-update-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      console.log(productId);
+    });
+  });
