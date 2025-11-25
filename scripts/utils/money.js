b5e158bf7ef48e5ce6af2cfe007feeef5279830d
ZiadGH <ziad97algawy@gmail.com>
2025-11-25 20:26:13 +0100

    Save data and generate HTML for payment Summary

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,6 +1,6 @@
 // function to make the eqation for the prices
 export function formatCurrency(priceCents){
-  return(priceCents / 100 ).toFixed(2);
+  return(Math.round(priceCents) / 100 ).toFixed(2);
 }
 
 export default formatCurrency;
